c905854e825d06f6fb72e3e7c59dde524d415bd4
Márcio Araujo <maritsu2015@gmail.com>
2016-10-17 23:35:10 -0200

    CORE-02
    Entidade user

---
+++ src/main/java/br/com/gears/jpastudy/entity/User.java
@@ -0,0 +1,68 @@
+package br.com.gears.jpastudy.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(
+	name="user", 
+	indexes = {@Index(columnList= "name, last_name", name="idx_user_name", unique=true)}
+)
+public class User implements Serializable{
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name ="id_user")
+	private Long id;
+	
+	@Column(nullable = false, length = 40)
+	private String name;
+	
+	@Column(name= "last_name", nullable = false, length = 75)
+	private String lastName;
+	
+	@Column(nullable = false, length = 60)
+	private String login;
+	
+	@Column(nullable = false, length = 100)
+	private String pass;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", lastName=" + lastName + ", login=" + login + ", pass=" + pass
+				+ "]";
+	}
+
+}
